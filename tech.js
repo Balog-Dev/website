fa08c3cf0d2eb29f720f40d4eb3441e4f7a4399d
Balog-dev <ridwanbalogun511@gail.com>
2022-02-19 12:49:45 +0100

    const

--- tech.js
+++ tech.js
@@ -1,11 +1,11 @@
-const sing_in_btn = document.querySelector('sign-in-btn');
-const sing_up_btn = document.querySelector('sign-up-btn');
-const container = document.querySelector('.container');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-sing_up_btn.addEventListener('click', ()=> {
-    container.classList.add('sign-up-mode');
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
 });
 
-sing_in_btn.addEventListener('click', ()=> {
-    container.classList.add('sign-in-mode');
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
